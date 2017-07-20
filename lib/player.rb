60a8aae5dfc4757c25beb3ed4fe7664840846cb1
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-20 14:47:29 +0200

    Implementing hit points completed

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,19 @@
 # this class will be storing information about player names and scores
 class Player
-  attr_reader :name
+  DEFAULT_HIT_POINTS = 60
+  attr_reader :name, :hit_points
 
-  def initialize(name)
+  def initialize(name, hit_points = DEFAULT_HIT_POINTS)
     @name = name
+    @hit_points = hit_points
   end
+
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hit_points -= 10
+  end
+  
 end
