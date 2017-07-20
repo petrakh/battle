8cfadbe0e7179177c0e221e5ee463528b4ef9666
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-20 18:43:41 +0200

    Skinnier controller thanks to new game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,16 @@
 # this class is responsible for attacking other players
 class Game
 
-  attr_reader :players
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+  end
 
-  def initialize
-    @players
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
   end
 
   def attack(player)
